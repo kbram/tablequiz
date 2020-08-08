fafcae445f8585dd42a0d7783bb023beda4ccd3c
kopi-dev <kopinathkopi96@gmail.com>
2020-08-08 09:25:33 +0530

    quiz-round

--- app/Http/Controllers/QuizRoundController.php
+++ app/Http/Controllers/QuizRoundController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\QuizRound;
+use Validator;
+use Illuminate\Support\Str;
 
 class QuizRoundController extends Controller
-{
-    
+{     
+    public function store(Request $request){
+       
+        $validator = Validator::make($request->all(),
+        [
+            'round_name'                        => 'required',
+            'bg_image'                          => '',
+            
+        ],
+    );
+
+    if ($validator->fails()) {
+        return back()->withErrors($validator)->withInput();
+    }
+    $request->bg_image->store('background_image');
+    $quiz_round=new QuizRound;
+     $quiz_round->round_name = $request->input('round_name');
+     $quiz_round->round_slug = Str::slug($request->input('round_name'),'-');
+    $quiz_round->save();
+
+    return back()->with('success', trans('usersmanagement.createSuccess'));
+    }
 }
