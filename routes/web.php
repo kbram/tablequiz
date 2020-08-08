8250d848f4a52fa11444ece69499e2fe000e303b
kopi-dev <kopinathkopi96@gmail.com>
2020-08-08 10:10:57 +0530

    about us  and contact us page

--- routes/web.php
+++ routes/web.php
@@ -147,6 +147,13 @@ Route::get('quiz/slider', 'QuizController@slider');
 Route::get('quiz/add_round', 'QuizController@add_round');
 Route::get('quiz/add_round_2', 'QuizController@add_round_2');
 Route::get('quiz/setup', 'QuizController@setup');
+Route::post('round/store', 'QuizRoundController@store');
+Route::get('about_us',function(){
+    return view('about_us');
+});
+Route::get('contact_us',function(){
+    return view('contact_us');
+});
 //sam route can start here
 
 //christy route can start from here
