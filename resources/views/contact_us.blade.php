de350502c0555dc13e008da506093d0be9f1d80b
kopi-dev <kopinathkopi96@gmail.com>
2020-08-08 11:10:07 +0530

    contact_us

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -1,15 +1,99 @@
 @extends('layouts.tablequizapp')
 
 @section('content')
-<section class="container page__inner">
-	<div class="row">
-		<article class="col-12">
-			<h1 class="mb-4">Contact Us</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan ut nisl ac dictum. Pellentesque sem lacus, varius nec ex a, ullamcorper convallis arcu. Nam aliquet tincidunt orci, eu semper diam mattis dignissim. Praesent quis velit quis erat fermentum condimentum. Integer dapibus felis at urna luctus, et suscipit nunc bibendum. Integer fringilla lacus purus. Fusce bibendum quis enim quis efficitur. Proin consectetur rutrum nisi, et rhoncus tellus elementum non. Nunc eget est non neque lacinia vehicula. Donec ornare convallis faucibus. Suspendisse lacinia et eros a dignissim. Ut pellentesque risus et leo mollis, suscipit porta ante consectetur. Donec volutpat lacus consectetur nibh tempor laoreet.
-			</p>
-			
-		</article>
-	</div>
+<section class="mb-4 container page__inner">
+
+    <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+        a matter of hours to help you.</p>
+     
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-9 mb-md-0 mb-5">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control">
+                            <label for="name" class="">Your name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="email" name="email" class="form-control">
+                            <label for="email" class="">Your email</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Subject</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            <label for="message">Your message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="text-center text-md-left">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            </div>
+            <div class="status"></div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>San Francisco, CA 94126, USA</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p>+ 01 234 567 89</p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p>contact@mdbootstrap.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+    </div>
+
 </section>
 @endsection
 
