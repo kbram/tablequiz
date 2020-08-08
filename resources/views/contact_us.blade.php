a6a0409454b7e534fa01e0ae575cec00de9369c6
kopi-dev <kopinathkopi96@gmail.com>
2020-08-08 12:45:30 +0530

    contact_us

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -2,98 +2,96 @@
 
 @section('content')
 <section class="mb-4 container page__inner">
-
-    <!--Section heading-->
-    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-    <!--Section description-->
-    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+	 <!--Section heading-->
+	 <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+	<!--Section description-->
+	<p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
         a matter of hours to help you.</p>
-     
-    <div class="row">
+		         <div class="row">
 
-        <!--Grid column-->
-        <div class="col-md-9 mb-md-0 mb-5">
-            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+						<div class="col-sm-4">
+							<div class="vc_column-inner">
+								<div class="wpb_wrapper text-center">
+									<div id="mk-icon-box-5" class="mk-box-icon-2  jupiter-donut- box-align-center ">
+										<div class="mk-box-icon-2-icon size-48 "> 
+										<i class='fas fa-location-arrow' style='font-size:48px;color:white'></i>
+									       </div>
+										<h3 class="mk-box-icon-2-title">Address</h3>
+										<p class="mk-box-icon-2-content"><span style="color: #ffffff;"><b>Weifield Group Contracting</b><br>
+												6950 S. Jordan Road<br>
+												Centennial, CO 80112</span></p>
+										<p><span style="color: #ffffff;"><b>Industrial Division Office</b><br>
+												1270 Automation Drive #500<br>
+												Windsor, CO 80550</span></p>
+										<p><span style="color: #ffffff;"><b>Wyoming Office</b><br>
+												308 Southwest Dr Unit E<br>
+												Cheyenne, WY 82007</span></p>
+										
+									</div>
+									<div id="padding-6" class="mk-padding-divider jupiter-donut-visible-sm  jupiter-donut-clearfix"></div>
 
-                <!--Grid row-->
-                <div class="row">
+								</div>
+							</div>
+						</div>
 
-                    <!--Grid column-->
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control">
-                            <label for="name" class="">Your name</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
 
-                    <!--Grid column-->
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="email" name="email" class="form-control">
-                            <label for="email" class="">Your email</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
+						<div class="col-sm-4">
+							<div class="vc_column-inner">
+								<div class="wpb_wrapper text-center">
+									<div id="mk-icon-box-7" class="mk-box-icon-2  jupiter-donut- box-align-center ">
+										<div class="mk-box-icon-2-icon size-48"> 
+											<i class="fa fa-phone" style="font-size:48px;color:white"></i>
+										 </div>
+										<h3 class="mk-box-icon-2-title">Phone</h3>
+										<p class="mk-box-icon-2-content"><span style="color: #ffffff;"><b>Weifield Group Contracting</b><br>
+												303.428.2011 phone<br>
+												303.202.0466 facsimile</span></p>
+										<p><span style="color: #ffffff;"><b>Weifield 24/7 Service Department</b><br>
+												303.428.2011<br>
+												(Then press 2 for emergency calls)</span></p>
+										<p><span style="color: #ffffff;"><b>Industrial Division Office</b><br>
+												303.428.2011 phone<br>
+												303.202.0466 facsimile</span></p>
+										
+									</div>
+									<div id="padding-8" class="mk-padding-divider jupiter-donut-visible-sm  jupiter-donut-clearfix"></div>
 
-                </div>
-                <!--Grid row-->
+								</div>
+							</div>
+						</div>
 
-                <!--Grid row-->
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="md-form mb-0">
-                            <input type="text" id="subject" name="subject" class="form-control">
-                            <label for="subject" class="">Subject</label>
-                        </div>
-                    </div>
-                </div>
-                <!--Grid row-->
 
-                <!--Grid row-->
-                <div class="row">
+						<div class="col-sm-4">
+							<div class="vc_column-inner">
+								<div class="wpb_wrapper text-center">
+									<div id="mk-icon-box-9" class="mk-box-icon-2  jupiter-donut- box-align-center ">
+										<div class="mk-box-icon-2-icon size-48"> 
+											<i class="	fa fa-envelope" style="font-size:48px;color:white"></i> 
+										</div>
+										<h3 class="mk-box-icon-2-title">Email</h3>
+										<p class="mk-box-icon-2-content"><span style="color: #ffffff;"><b>Request for Proposal</b></span><br>
+											<span style="color: #ffffff;"> <a style="color: #ffffff;" href="mailto:info@weifieldcontracting.com">Info@weifieldgroup.com</a></span></p>
+										<p><span style="color: #ffffff;"><b>All Bid Opportunities</b></span><br>
+											<span style="color: #ffffff;"> <a style="color: #ffffff;" href="mailto:estimating@weifieldgroup.com">estimating@weifieldgroup.com</a></span></p>
+										<p><span style="color: #ffffff;"><b>Electrical Service Calls</b></span><br>
+											<span style="color: #ffffff;"> <a style="color: #ffffff;" href="mailto:service@weifieldcontracting.com">service@weifieldcontracting.com</a></span></p>
+										<p><span style="color: #ffffff;"><b>Employment Opportunities</b></span><br>
+											<span style="color: #ffffff;"> <a style="color: #ffffff;" href="mailto:careers@weifieldcontracting.com">careers@weifieldcontracting.com</a></span></p>
+									</div>
+									<div id="padding-10" class="mk-padding-divider jupiter-donut-visible-sm  jupiter-donut-clearfix"></div>
 
-                    <!--Grid column-->
-                    <div class="col-md-12">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
 
-                        <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                            <label for="message">Your message</label>
-                        </div>
 
-                    </div>
-                </div>
-                <!--Grid row-->
-
-            </form>
-
-            <div class="text-center text-md-left">
-                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
-            </div>
-            <div class="status"></div>
-        </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
-        <div class="col-md-3 text-center">
-            <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                    <p>San Francisco, CA 94126, USA</p>
-                </li>
-
-                <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                    <p>+ 01 234 567 89</p>
-                </li>
-
-                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                    <p>contact@mdbootstrap.com</p>
-                </li>
-            </ul>
-        </div>
-        <!--Grid column-->
-
-    </div>
-
+	</div>
+</div>
 </section>
 @endsection
 
