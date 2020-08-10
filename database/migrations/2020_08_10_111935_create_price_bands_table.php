d30e5ea8010c804fc44cb7651c5467f1c87dec87
Christy <christygeorge1995@gmail.com>
2020-08-10 17:33:00 +0530

    price_bands

---
+++ database/migrations/2020_08_10_111935_create_price_bands_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePriceBandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price_bands', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('from')->unigned();
+            $table->integer('to')->unsigned();
+            $table->string('band_type');
+            $table->integer('cost')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prize_bands');
+    }
+}
