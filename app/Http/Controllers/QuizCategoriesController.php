c8c53b95f838d4cca094eed919a87cd9535c8c15
manchulani <manchulani97@gmail.com>
2020-08-11 10:58:38 +0530

    quiz category image upload

--- app/Http/Controllers/QuizCategoriesController.php
+++ app/Http/Controllers/QuizCategoriesController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 use Validator;
 
+use Auth;
+use Image;
 use Illuminate\Http\Request;
 use App\Model\QuizCategory;
 use Illuminate\Support\Str;
+
+use File;
+
 
 class QuizCategoriesController extends Controller
 {
@@ -20,7 +25,6 @@ class QuizCategoriesController extends Controller
 
     public function store(Request $request)
     {
-        //dd($request);
 
         $validator = Validator::make($request->all(),
         [
@@ -34,17 +38,33 @@ class QuizCategoriesController extends Controller
             return back()->withErrors($validator)->withInput();
         }
         
-        //$request->category_image->store('category_image');
 
         $categories=new QuizCategory;
         $categories -> category_name     =  $request->input('category_name');
         $categories -> category_slug = Str::slug($request->input('category_name'),'-');
-        $categories -> category_image = $request->file('category_image')->store('category_image');
+        $categories -> category_image = $request->file('category_image');
+
+        if ($request->hasFile('category_image')) {
+
+            $category_image = $request->file('category_image');
+            $filename = 'category_image.'.$category_image->getClientOriginalExtension();
+            $save_path = storage_path().'/admin/categories/id/'.$categories->id.'/uploads/images/category_images/';
+            $path = $save_path.$filename;
+            $public_path = '/images/'.$categories->id.'/category_image/'.$filename;
+
+            // Make the user a folder and set permissions
+            File::makeDirectory($save_path, $mode = 0755, true, true);
+
+            // Save the file to the server
+            Image::make($category_image)->resize(300, 300)->save($save_path.$filename);
+
+            // Save the public image path
+
         
-       // dd($categories);
+        }
+
        $categories->save();
        
-       // return('admin.categories');
        return redirect()->back();
     }
 
