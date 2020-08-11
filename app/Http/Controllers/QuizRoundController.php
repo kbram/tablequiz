c4a7a049bc26674c94a1f9525a6295832723cf19
kopi-dev <kopinathkopi96@gmail.com>
2020-08-11 10:08:17 +0530

    background_image

--- app/Http/Controllers/QuizRoundController.php
+++ app/Http/Controllers/QuizRoundController.php
@@ -6,28 +6,49 @@ use Illuminate\Http\Request;
 use App\Models\QuizRound;
 use Validator;
 use Illuminate\Support\Str;
+use App\Models\QuizRoundImage;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Session;
 
 class QuizRoundController extends Controller
 {     
     public function store(Request $request){
        
-        $validator = Validator::make($request->all(),
-        [
-            'round_name'                        => 'required',
-            'bg_image'                          => '',
-            
-        ],
-    );
+           
+       if ($request->hasFile('bg_image')) {
+        if ($request->file('bg_image')->isValid()) {
+            $validated = $request->validate([
+                'round_name' => 'required',
+                'bg_image' => 'mimes:jpeg,png|max:2048',
+            ]);
+            $extension = $request->bg_image->extension();
+            $time=time();
+            $request->bg_image->storeAs('/public', $validated['round_name'].$time.".".$extension);
+            $url = Storage::url($validated['round_name'].$time.".".$extension);
 
-    if ($validator->fails()) {
-        return back()->withErrors($validator)->withInput();
-    }
-    $request->bg_image->store('background_image');
-    $quiz_round=new QuizRound;
-     $quiz_round->round_name = $request->input('round_name');
-     $quiz_round->round_slug = Str::slug($request->input('round_name'),'-');
-    $quiz_round->save();
+            $round = QuizRound::create([
+                'round_name' => $validated['round_name'],
+                'round_slug' => Str::slug($validated['round_name'],'-'),
+             ]);
+             
+             $round->save();
+           $get_round_id=QuizRound::where('round_name','=',$round->round_name)->first();
+           
+            $file = QuizRoundImage::create([
+               'name' => $validated['round_name'].$time,
+                'url' => $url,
+                'round_id' => $get_round_id->id,
+            ]);
+         
 
-    return back()->with('success', trans('usersmanagement.createSuccess'));
+           $file->save();
+        
+           
+            return redirect()->back();
+        }
     }
+    abort(500, 'Could not upload image :(');
+    }
+
+   
 }
--- app/Models/QuizRound.php
+++ app/Models/QuizRound.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class QuizRound extends Model
-{
-    //
+{    
+    protected $table = 'quiz_rounds';
+   
+    protected $fillable = [
+        'round_name','round_slug'
+    ];
+
+    public function getImage()
+    {
+        return $this->hasOne('App\Models\QuizRoundImage');
+    } 
 }
