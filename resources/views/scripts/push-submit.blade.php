2ec9a601cb5d66ac46df3e81a47494302477e6d1
bavaram <nkbram95@gmail.com>
2020-09-16 12:08:41 +0530

    start_part_timer

--- resources/views/scripts/push-submit.blade.php
+++ resources/views/scripts/push-submit.blade.php
@@ -4,20 +4,21 @@ $(document).ready(function() {
        e.preventDefault();
       $('.quiz__slider .quiz__single_question__container').each(function(){
       
+
        var answer="";
        var answer_id="";
 
        var media_type="";
        var media_path="";
        var media_link="";
-
+       var time=0;
 
        var current=$(this);
          var hidden=current.attr('aria-hidden');
          if(hidden=="false"){
             var question =current.find('.question > span:nth-child(2)').text();
             
-            var time =current.find('.question-timer').val();
+            time =current.find('.question-timer').val();
             var round =current.find('.question-round').val();
             var type =current.find('.question-type').val();
             var user =current.find('.question-user').val();
@@ -44,50 +45,35 @@ $(document).ready(function() {
            current.find('.question-media-ink').each(function(){
                 media_link+=$(this).val()+"**";
             });
-// console.log('hi'+question);
-//             $('#push-submit-form-question').val(question);
-//             $('#push-submit-form-answer').val( answer);
-//             $('#push-submit-form-time').val(time);
-//             $('#push-submit-form-media').val(media_type);
-//             $('#push-submit-form-media_link').val(media_link);
-//             $('#push-submit-form-media_path').val(media_path);
 
-//             $('#push-submit-form-answerId').val(answer_id);
-//             $('#push-submit-form-questionId').val(id);
-//             $('#push-submit-form-roundId').val(round);
-//             $('#push-submit-form-quizId').val(time);
-
-//             $('#push-submit-form-type').val(time);
-           
-           
              
            
-            $.ajax({
-        type: "POST",
-        url: "/quiz/run_quiz",
-        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-        data:{
-           question:question,
-           answer:answer,
-           media_type:media_type,
-           media_link:media_link,
-           media_path:media_path,
+         $.ajax({
+            type: "POST",
+            url: "/quiz/run_quiz",
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            data:{
+                  question:question,
+                  answer:answer,
+                  media_type:media_type,
+                  media_link:media_link,
+                  media_path:media_path,
 
-           answerId:answer_id,
-           questionId:id,
-           roundId:round,
-           quizId:id,
-           time:time,
+                  answerId:answer_id,
+                  questionId:id,
+                  roundId:round,
+                  quizId:id,
+                  time:time,
 
-        },
-        success: function(data) {
+             },
+            success: function(data) {
+               
+            },
+         error: function(result,error) {
          
-        },
-        error: function(result,error) {
-      
-        },
+         },
 
-      });
+         });
          }
          
          
@@ -105,7 +91,22 @@ $(document).ready(function() {
          var hidden=current.attr('aria-hidden');
          if(hidden=="false"){
             var time =current.find('.question-timer').val();
-            $('.countdown__time').text(time);
+            sec=time;
+            var min     = Math.floor(sec / 60),
+				remSec  = sec % 60;
+			
+            if (remSec < 10) {
+               
+               remSec = '0' + remSec;
+            
+            }
+            if (min < 10) {
+               
+               min = '0' + min;
+            
+            }
+            
+            $('.countdown__time').text(min + ":" + remSec);
          }
 
    });
@@ -122,8 +123,14 @@ $(document).ready(function() {
    });
 });
 
-   
+$('#push-submit-pause').click(function(e){
+       e.preventDefault();  
 
+});
+$('#push-submit-stop').click(function(e){
+       e.preventDefault();  
+
+});
 });
 
 
