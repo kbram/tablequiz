579cb28a7ea141d60e42d60f05efc86ecfa1ac18
kopinath <kopinathkopi96@gmail.com>
2020-10-08 13:49:22 +0530

    view card

--- public/site_design/js/scripts.js
+++ public/site_design/js/scripts.js
@@ -931,25 +931,91 @@ jQuery(document).ready(function ($) {
         });
     });
 
-    //     $('#publish-quiz').click(function(e){ console.log('hi round');
+        $('.view-card').click(function(e){ console.log('hi round');
 
-    //        e.preventDefault();
-    //     $.ajax({
-    //         data: $('#add_round').serialize(),
-    //         type: 'post',
-    //         url: $('#add_round').attr('action'),
-    //         headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-    //         success: function(response) {
-    //             console.log('success');
-    //             $('#publishQuizModal').modal("show");
+        //    e.preventDefault();
+        // $.ajax({
+        //     data: $('#add_round').serialize(),
+        //     type: 'post',
+        //     url: $('#add_round').attr('action'),
+        //     headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+        //     success: function(response) {
+        //         $('#publishQuizModal').modal("show");
 
-    // 		},
-    // 		error: function(result,error) {
-    //             console.log('errror');
-    // 		    $('#modal__payment').modal("show");
-    // 		   },
-    //     });
-    //     return false;
+    	// 	},
+    	// 	error: function(result,error) {
+        //         console.log('errror');
+    	// 	   //$('#publishQuizModal').modal("show");
+    	// 	   },
+        // });
 
-    //    });
+        $.ajax({
+            type: "POST",
+            url: "/payment",
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                    "content"
+                ),
+            },
+            data: {
+                count: sessionStorage.count,
+            },
+            success: function (data) {
+                $('#publishQuizModal').modal("show");
+                var participants = data.participants.no_of_participants;
+                var participants_cost = data.participants_cost[0].cost;
+                var questions_cost = 0;
+                console.log(typeof participants_cost);
+                $("#modal__payment")
+                    .find(".no-participants td:nth-child(2)")
+                    .text(participants);
+                $("#modal__payment")
+                    .find(".no-participants td:nth-child(3)")
+                    .text(participants_cost);
+
+                $("#modal__payment")
+                    .find(".suggested-questions td:nth-child(2)")
+                    .text(sessionStorage.count);
+
+                if (data.question_cost != null) {
+                    $("#modal__payment")
+                        .find(".suggested-questions td:nth-child(3)")
+                        .text(data.question_cost.cost);
+                    questions_cost = data.question_cost.cost;
+                } else {
+                    $("#modal__payment")
+                        .find(".suggested-questions td:nth-child(3)")
+                        .text(0);
+                    questions_cost = 0;
+                }
+
+                var customised_backgrounds_details = $(
+                    "#modal__payment"
+                )
+                    .find(".customised-backgrounds td:nth-child(2)")
+                    .text(data.bg_image);
+                var customised_backgrounds_price = $("#modal__payment")
+                    .find(".customised-backgrounds td:nth-child(3)")
+                    .text(data.bg_image_cost.cost);
+
+                $("#modal__payment")
+                    .find(".total-cost td:nth-child(2)>strong")
+                    .text(
+                        Number(participants_cost) +
+                            Number(questions_cost) +
+                            Number(data.bg_image_cost.cost)
+                    );
+                var total_card =
+                    Number(participants_cost) +
+                    Number(questions_cost) +
+                    Number(data.bg_image_cost.cost);
+                $("#card_total").val(total_card);
+            },
+            error: function (result, error) {},
+        });
+        return false;
+
+       });
+
+       
 });
