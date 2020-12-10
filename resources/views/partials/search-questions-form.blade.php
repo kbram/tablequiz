c6ac282f3462e8006fe44e0a38ed0cdd058b0048
Kirushanth <skirush28@gmail.com>
2020-12-10 18:04:42 +0530

    search box css issues sorted

--- resources/views/partials/search-questions-form.blade.php
+++ resources/views/partials/search-questions-form.blade.php
@@ -1,8 +1,10 @@
 <div class="p-0">
 <form action="" method="" class="p-0 d-flex" id="search_questions">
     {!! csrf_field() !!}
-        <input type="text" list="quizzes" name="question_search_box" id="question_search_box" placeholder="Search..." class="form-control mb-2 mt-n2 input-sm .col-lg-*"> 
-        <button type="submit" class="btn btn-secondary mb-2 mt-n2" id="search_trigger"><i class="fas fa-search" aria-hidden="true"></i> </button>
+        <input type="text" list="quizzes" name="question_search_box" id="question_search_box" placeholder="Search..." class="form-control mb-2 mt-n2 input-sm .col-lg-*" style="border-top-right-radius: 0px !important;
+        border-bottom-right-radius: 0px !important;"> 
+        <button type="submit" class="btn btn-secondary mb-2 mt-n2" style="border-top-left-radius: 0px !important;
+        border-bottom-left-radius: 0px !important;" id="search_trigger"><i class="fas fa-search" aria-hidden="true"></i> </button>
 </form> 
 </div>
 
--- resources/views/partials/search-quizzes-form.blade.php
+++ resources/views/partials/search-quizzes-form.blade.php
@@ -1,7 +1,9 @@
 <div class="p-0">
 <form action="" method="" class="p-0 d-flex" id="search_quizzes">
     {!! csrf_field() !!}
-        <input type="text" name="quiz_search_box" id="quiz_search_box" placeholder="Search..." class="form-control mb-2 mt-n2 input-sm .col-lg-*"> 
-        <button type="submit" class="btn btn-secondary mb-2 mt-n2" id="search_trigger"><i class="fas fa-search" aria-hidden="true"></i> </button>
+        <input type="text" name="quiz_search_box" id="quiz_search_box" placeholder="Search..." class="form-control mb-2 mt-n2 input-sm .col-lg-*" style="border-top-right-radius: 0px !important;
+        border-bottom-right-radius: 0px !important;"> 
+        <button type="submit" class="btn btn-secondary mb-2 mt-n2" style="border-top-left-radius: 0px !important;
+        border-bottom-left-radius: 0px !important;" id="search_trigger"><i class="fas fa-search" aria-hidden="true"></i> </button>
 </form> 
 </div>
